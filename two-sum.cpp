e9e1bbca6e95626bbe68e9a255d6c3b348c83e58
Angela Lin <angelala960520@gmail.com>
2026-07-03 22:45:52 +0800

    Add files via upload

---
+++ two-sum.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+
+        if (mp.find(complement) != mp.end()) {
+            return {mp[complement], i};
+        }
+
+        mp[nums[i]] = i;
+    }
+
+    return {};
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int target;
+    cin >> target;
+
+    vector<int> result = twoSum(nums, target);
+
+    if (result.empty()) {
+        cout << "No answer" << endl;
+    } else {
+        cout << result[0] << " " << result[1] << endl;
+    }
+
+    return 0;
+}
